05801aaedbc787a63d08b6dec26ce187fdc96e35
zerocodebit254 <muhiugikonyo@gmail.com>
2021-11-03 00:29:35 +0300

    Edited about & index pages, pages.views & models.py
    Made content loading more dynamic  for index and about pages.
    Removed unnessecary  bathroom field in  listings.models.py.

--- real_estate_proj/listings/models.py
+++ real_estate_proj/listings/models.py
@@ -16,7 +16,6 @@ class Listing(models.Model):
     description = models.TextField(blank=True)
     price = models.IntegerField()
     bedrooms = models.IntegerField()
-    bathrooms = models.IntegerField()
     bathrooms = models.DecimalField(max_digits=2, decimal_places=1)
     garage = models.IntegerField(default=0)
     sqft = models.IntegerField()
--- real_estate_proj/pages/views.py
+++ real_estate_proj/pages/views.py
@@ -1,10 +1,33 @@
 from django.shortcuts import render
+from listings.models import Listing
+from realtors.models import Realtor
+
 
 # Create your views here.
 
+
 def index(request):
-    return render(request, 'pages/index.html')
+    # get all listings, order by listdate desc, filter by ispublished
+    #[:3] limits no. of listings to 3
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3] 
+
+    context = {
+        'listings':listings
+    }
+
+    return render(request, 'pages/index.html',context)
 
 def about(request):
-    return render(request, 'pages/about.html')
+    # get all realtors
+    realtors = Realtor.objects.order_by('-hire_date')
+
+    # get MVP
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+
+    context = {
+        'realtors': realtors,
+        'mvp_realtors': mvp_realtors
+    }
+
+    return render(request, 'pages/about.html', context)
 
